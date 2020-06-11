5cc1111601acf63f49382975ea06513d3bc93ff9
Elias Norrby <elias.norrby@gmail.com>
2020-06-11 15:38:51 +0200

    L4: List Rendering

--- intro-to-vue/main.js
+++ intro-to-vue/main.js
@@ -1,9 +1,20 @@
 var app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg',
+    product: "Socks",
+    image: "./assets/vmSocks-green-onWhite.jpg",
     inventory: 9,
-    onSale: true
-  }
-})
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+      },
+    ],
+  },
+});
