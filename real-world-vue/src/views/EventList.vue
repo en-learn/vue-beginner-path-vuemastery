a664b2ae7054502396a4149131fe522c217856ca
Elias Norrby <elias.norrby@gmail.com>
2020-06-22 14:41:01 +0200

    L6: Single File Vue Components

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <h1>Events listing</h1>
+    <EventCard />
 
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }">Show Event #1</router-link>
   </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
