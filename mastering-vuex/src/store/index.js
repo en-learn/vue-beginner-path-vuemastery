8f94b6852280886110a23c1556a66e38791c571e
Elias Norrby <elias.norrby@gmail.com>
2020-06-28 12:39:01 +0200

    L3: State & Getters

--- mastering-vuex/src/store/index.js
+++ mastering-vuex/src/store/index.js
@@ -1,11 +1,32 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'edudation',
+      'food',
+      'community',
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+    ],
+  },
   mutations: {},
   actions: {},
-  modules: {}
-});
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  },
+  modules: {},
+})
