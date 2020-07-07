28e443da27f73a3d2c55b8179f43f7967d847188
Elias Norrby <elias.norrby@gmail.com>
2020-07-07 10:50:54 +0200

    L4: Mutations & Actions Pt. 1

--- mastering-vuex/src/store/index.js
+++ mastering-vuex/src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import EventService from '@/services/EventService'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -21,12 +23,22 @@ export default new Vuex.Store({
       { id: 3, title: '...', organizer: '...' },
     ],
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    },
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    },
+  },
   getters: {
     getEventById: state => id => {
       return state.events.find(event => event.id === id)
-    }
+    },
   },
   modules: {},
 })
