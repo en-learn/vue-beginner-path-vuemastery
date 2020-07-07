9d1e2523784555f776444b7f85a571ede8ddd2c2
Elias Norrby <elias.norrby@gmail.com>
2020-07-07 11:13:28 +0200

    L5: Mutations & Actions Pt. 2

--- mastering-vuex/src/views/EventList.vue
+++ mastering-vuex/src/views/EventList.vue
@@ -3,30 +3,43 @@
     <h1>Events listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <BaseIcon />
+    <router-link
+      v-if="page != 1"
+      :to="{name: 'event-list', query: {
+      page: page -1
+      }}"
+      rel="prev"
+    >Prev Page</router-link>
+      |
+    <router-link
+      v-if="eventsTotal > this.page * 3"
+      :to="{name: 'event-list', query: {
+      page: page +1
+      }}"
+      rel="next"
+    >Next Page</router-link>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService'
+import { mapState } from 'vuex'
 
 export default {
   components: {
-    EventCard
-  },
-  data() {
-    return {
-      events: []
-    }
+    EventCard,
   },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log('There was an error: ', error.response)
-      })
-  }
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page,
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    ...mapState(['events', 'eventsTotal']),
+  },
 }
 </script>
